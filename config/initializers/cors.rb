197ec7fc51489d38640142f0b2e8893015a15d37
Matt Haws <matthaws@gmail.com>
2018-02-28 16:41:36 -0500

    Add production url to allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'http://localhost:3001', 'http://douze-points.s3-website-us-east-1.amazonaws.com'
 
     resource '*',
       headers: :any,

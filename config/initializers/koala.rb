f2ae5b1b0d8175d6b0d6553daea76365f27dddbb
Matt Haws <matthaws@gmail.com>
2018-03-18 16:44:48 -0400

    Allows backend to access both productio and development version of app

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,6 +1,11 @@
 Koala.configure do |config|
-  config.app_id = ENV["FACEBOOK_KEY"]
-  config.app_secret = ENV["FACEBOOK_SECRET"]
+  if Rails.env.production?
+    config.app_id = ENV["FACEBOOK_PRO_KEY"]
+    config.app_secret = ENV["FACEBOOK_PRO_SECRET"]
+  else
+    config.app_id = ENV["FACEBOOK_DEV_KEY"]
+    config.app_secret = ENV["FACEBOOK_DEV_SECRET"]
+  end
   # See Koala::Configuration for more options, including details on how to send requests through
   # your own proxy servers.
 end

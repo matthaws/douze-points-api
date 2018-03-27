a4beace3e6834d2e18e34aa9e9659d078be6d085
Matt Haws <matthaws@gmail.com>
2018-03-27 13:51:08 -0400

    Add 2010-2015 seeds and parser methods

--- db/seed_parser.rb
+++ db/seed_parser.rb
@@ -1,33 +1,35 @@
-def parse_seeds
-  seeds = File.readlines('db/raw_seeds.txt').map(&:chomp)
-  contest = nil
-  seeds.each do |seed|
-    next if seed == ""
-    seed_parts = seed.split(" ")
-    if seed_parts[1] == "Final,"
-      seed_parts.map! { |part| part.chomp(",") }
-      host_country = Country.find_by_name(seed_parts[2])
-      contest = Contest.create({
-        year: seed_parts.first,
-        location: seed_parts[3],
-        host_country: host_country
-      })
+def parse_contests
+  CSV.foreach("db/contests.csv") do |contest|
+    next if contest.first == "Year"
+    con = Contest.find_by_year(contest.first)
+    unless con
+      con = Contest.new
+      con.year = contest.first
+      con.host_country = Country.find_by_name(contest[1])
+      con.location = "#{contest[2]}, #{contest[1]}"
+      con.save!
+    end
+  end
+end
 
-    else
-      final_ranking, final_score, order, country = seed_parts
-      country = Country.find_by_name(country)
-      artist, song = seed_parts[4..-1].join(" ").split(" / ")
-      entry = Entry.create({
-        artist: artist,
-        song_title: song,
-        final_score: final_score,
-        final_ranking: final_ranking,
-        finalist: true,
-        country: country,
-        contest: contest
-        })
+def parse_years(year_file)
+  CSV.foreach("db/#{year_file}.csv") do |entry|
+    next if entry.first == "Final Ranking"
+    new_entry = Entry.new
+    new_entry.final_ranking = entry.first
+    new_entry.contest = Contest.find_by_year(entry.last)
 
-        entry.contest.winning_entry = entry if entry.final_ranking == 1
+    new_entry.country = Country.find_by_name(entry[3])
+    new_entry.final_score = entry[1]
+    new_entry.song_title = entry[5]
+    new_entry.artist = entry[4]
+    new_entry.video_url = entry[6]
+    new_entry.finalist = true
+
+    new_entry.save
+    if entry.first.to_i == 1
+      new_entry.contest.winning_entry = new_entry
+      new_entry.contest.save
     end
   end
 end
